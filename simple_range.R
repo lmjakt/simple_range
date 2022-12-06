f35172ade08fc3ae7d955135d802d85fb55268e2
Martin Jakt <lmjakt@gmail.com>
2022-12-06 10:43:37 +0100

    Added arrange_lines functions to simple_range.R

--- simple_range.R
+++ simple_range.R
@@ -1,7 +1,8 @@
-## requires dyn.load("simple_range.so"), but this can't be
-## carried out here as the location is unknown.
-## there is a way of obtaining the location of this file, but I forget
-## how to do it at the moment. Should fix!
+## requires dyn.load("simple_range.so")
+## this performs some magic that should not be necessary if created
+## as a package
+dyn.load( paste(dirname(sys.frame(1)$ofile), "simple_range.so", sep="/") )
+dyn.load( paste(dirname(sys.frame(1)$ofile), "arrange_lines.so", sep="/") )
 
 rangesOverlap <- function(a, b){
     if(!is.numeric(a) || !is.numeric(b))
@@ -23,3 +24,18 @@ pointsInRanges <- function( ranges, points ){
     .Call( "points_in_ranges", ranges, points )
 }
 
+## x should be a matrix with two columns
+## if no columns with name x0 and x1, then the
+## first two columns will be taken to represent x0 and x1
+## respectively.
+arrange.lines <- function(x){
+    if(!is.matrix(x) || !is.numeric(x) || ncol(x) < 2)
+        stop("x should be a numeric matrix with at least two columns")
+    if(!all(c('x0', 'x1') %in% colnames(x)))
+        colnames(x)[1:2] <- c('x0', 'x1')
+    if(any( x[,'x0'] >= x[,'x1'] ))
+        stop("all x0 values must be smaller than x1")
+    if(any(is.na(x[,c('x0', 'x1')])))
+       stop("NA values not allowed")
+    .Call("arrange_lines", as.double(x[,'x0']), as.double(x[,'x1']))
+}
